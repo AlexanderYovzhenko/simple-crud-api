25acacac3c48240ca88c6a3f013e7a6d1d7b86f3
Alexander <renyreny125tt@gmail.com>
2021-11-25 16:20:53 +0300

    feat: parse request

---
+++ src/parse-url/parse-delete-request.js
@@ -0,0 +1,22 @@
+import * as path from 'path';
+
+import { dataBase } from '../database.js';
+import { UrlError, PersonError } from '../errors/users-errors.js';
+
+
+export const parseDeleteRequest = (req, res) => {
+  const url = path.basename(req.url);
+  const urlParse = path.parse(req.url);
+
+  if(url !== 'person') { 
+    if(dataBase('GetId', urlParse.base)) {
+      dataBase('Delete', urlParse.base);
+      res.statusCode = 204;  
+      res.end();  
+    } else {
+      throw new PersonError(`{"Message": "Person is not find with id" "${urlParse.base}"}`, 404);
+    }; 
+  } else {
+    throw new UrlError('{"Message": "Url is not correct"}', 404);
+  };
+};
